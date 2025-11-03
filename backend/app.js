ea0e027b7e58f8d1a5dce54850d8d6294333f590
Hades <vishal1807singh@gmail.com>
2025-11-04 01:47:01 +0530

    Implement routing and authentication features; add login, registration, and password recovery pages; update package dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 configDotenv();
 
 const app = express();
-
+app.use(
+   cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+)
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,19 @@
+import LogIn from "./component/LogIn.page.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Home from "./pages/Home.jsx"
+import Register from "./pages/Register.jsx"
+import Forgotpassword from'./pages/Forgotpassword.jsx'
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div className='text-6xl'>App</div>
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/registerUser" element={<Register />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/ForgotPassword" element={<Forgotpassword />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
   )
 }
 
